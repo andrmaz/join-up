50ea5f851c78db96e03300e4b175b96a870824fe
andrmaz <mazbsorz@gmail.com>
2021-02-14 10:09:04 +0100

    Initialize typescript

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+
+interface Props {
+
+}
+
+const Home: NextPage<Props> = () => {
+  return (
+    <div>
+      <Head>
+        <title>Project Zone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const response = await fetch('')
+  const data = await response.json()
+  return {
+    props: {
+    },
+  }
+}
