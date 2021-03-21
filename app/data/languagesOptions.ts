b5a044d5186eddbdde9123283d2663f5dca1c517
andrmaz <mazbsorz@gmail.com>
2021-03-21 17:56:40 +0100

    Implementing react select technologies and languages at signup page

---
+++ app/data/languagesOptions.ts
@@ -0,0 +1,13 @@
+export const languages = [
+    {value: 'english', label: 'English'},
+    {value: 'spanish', label: 'Spanish'},
+    {value: 'french', label: 'French'},
+    {value: 'italian', label: 'Italian'},
+    {value: 'mandarin', label: 'Mandarin'},
+    {value: 'hindi', label: 'Hindi'},
+    {value: 'arabic', label: 'Arabic'},
+    {value: 'bengali', label: 'Bengali'},
+    {value: 'russian', label: 'Russian'},
+    {value: 'portuguese', label: 'Portuguese'},
+    {value: 'indonesian', label: 'Indonesian'},
+]
