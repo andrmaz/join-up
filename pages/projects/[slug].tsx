abb41cf6d14b70db3eb2d2b31db4b47f7adc7454
andrmaz <mazbsorz@gmail.com>
2021-04-05 23:53:29 +0200

    Checking if user is the project owner

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,13 +1,29 @@
 import {NextPage, GetServerSideProps, InferGetServerSidePropsType} from 'next'
+import Head from 'next/head'
 import axios from 'axios'
 import {parseCookies} from '@utils/parseCookies'
 import {Params} from 'next/dist/next-server/server/router'
+import {useAuthState} from '@hooks/useAuthState'
 
 const Slug: NextPage = ({
   project,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log('%cProject Data', 'color: tomato', project)
-  return <div>Slug Page</div>
+  console.log('%cProject Data', 'color: tomato', project.owner)
+  const {user} = useAuthState()
+  console.log('%cUser Info', 'color: green', user?._id)
+  return (
+    <div className='min-h-screen'>
+      <Head>
+        <title>Project</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className='h-screen pt-20 container'>
+        <section className='h-full p-12'>
+          {project.owner === user?._id && <span>Add a new Post</span>}
+        </section>
+      </main>
+    </div>
+  )
 }
 
 export default Slug
