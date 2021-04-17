d6921a7130b3115247692e7984c53164d7af25ba
andrmaz <mazbsorz@gmail.com>
2021-04-17 18:04:40 +0200

    Initializing focus placement inside the dialog

--- app/components/Position/Modal.tsx
+++ app/components/Position/Modal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Portal from '@components/Portal/Portal'
-//import useRefCallback from '@hooks/ref/useRefCallback'
+import useRefCallback from '@hooks/ref/useRefCallback'
 
 const PositionModal = ({
   showModal,
@@ -10,7 +10,7 @@ const PositionModal = ({
   setShowModal: (state: boolean) => void
 }): JSX.Element => {
   //* ref will be a callback function instead of a Ref Object
-  //const [ref] = useRefCallback()
+  const [ref] = useRefCallback()
   const handleCancel = (e: React.MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault()
     setShowModal(false)
@@ -20,21 +20,22 @@ const PositionModal = ({
       {showModal && (
         <Portal>
           <section
-            role='dialog'
-            aria-modal={true}
+            id='dialog_layer'
             className='fixed top-0 left-0 h-screen w-screen bg-gray-500 bg-opacity-50 z-50'
           >
             <article
-              role='alertdialog'
-              aria-label='alertdialog'
+              id='dialog'
+              role='dialog'
+              aria-label='dialog'
               aria-labelledby='dialog_label'
-              aria-describedby=''
+              aria-modal={true}
+              aria-describedby='dialog_label'
               className='fixed h-3/5 w-1/2 top-1/4 right-1/4 bg-white border-black border-2 rounded p-2 '
             >
               <h2
                 id='dialog_label'
                 tabIndex={-1}
-                //ref={ref}
+                ref={ref}
                 className='focus:border-yellow-600'
               >
                 Add a new position
