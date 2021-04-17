8c2935ed7aa3637b6b69e6abdf4e6c14487b1515
andrmaz <mazbsorz@gmail.com>
2021-04-17 17:45:28 +0200

    Implemented Modal Component

--- app/components/Project/Badge.tsx
+++ app/components/Project/Badge.tsx
@@ -1,14 +1,29 @@
+import * as React from 'react'
+import {useAuthState} from '@hooks/auth/useAuthState'
+import PositionModal from '@components/Position/Modal'
 import type {IProjectData} from 'app/types/project'
 
 const ProjectBadge = ({
   name,
   projectURL,
   description,
+  owner,
 }: IProjectData): JSX.Element => {
+  const {user} = useAuthState()
+  const [showModal, setShowModal] = React.useState<boolean>(false)
   return (
     <div className='h-28 w-full border-b-2 border-gray-400'>
-      <header className='h-1/4 w-full'>
+      <header className='h-1/4 w-full flex justify-between'>
         <h2 className='font-black text-xl'>{name}</h2>
+        {user?._id === owner && (
+          <button
+            tabIndex={0}
+            className='bg-blue-800 text-white p-1 rounded'
+            onClick={() => setShowModal(true)}
+          >
+            Add a new position
+          </button>
+        )}
       </header>
       <article className='h-3/4'>
         <a className='text-sm' href={projectURL}>
@@ -16,6 +31,7 @@ const ProjectBadge = ({
         </a>
         <p className='text-sm'>{description}</p>
       </article>
+      <PositionModal showModal={showModal} setShowModal={setShowModal} />
     </div>
   )
 }
