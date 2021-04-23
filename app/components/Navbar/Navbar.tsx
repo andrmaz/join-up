e1778a63f17c4a4c0703d2598f6acaf73a6f7616
andrmaz <mazbsorz@gmail.com>
2021-04-23 11:30:09 +0200

    Removing collaborators route

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,15 +6,12 @@ const Navbar = (): JSX.Element => {
   const [tooltip, showTooltip] = React.useState(false)
   return (
     <nav className='fixed top-0 flex justify-between items-center h-20 w-screen bg-blue-800 text-white z-50'>
-      <div className='inline-flex justify-evenly w-5/12'>
+      <div className='inline-flex justify-evenly w-3/12'>
         <Link href='/'>
           <a className='flex-initial'>Logo</a>
         </Link>
         <Link href='/projects'>
           <a className='flex-initial'>Projects</a>
-        </Link>
-        <Link href='/collabs'>
-          <a className='flex-initial'>Collaborators</a>
         </Link>
       </div>
       <div className='inline-flex justify-around items-center w-2/12 h-full'>
