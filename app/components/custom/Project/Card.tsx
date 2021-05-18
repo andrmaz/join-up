b81e338ab748bac93f8c96c965ca91d74c621ac5
andrmaz <mazbsorz@gmail.com>
2021-05-18 22:30:22 +0200

    Added edit button to project preview

--- app/components/custom/Project/Card.tsx
+++ app/components/custom/Project/Card.tsx
@@ -6,33 +6,30 @@ const ProjectCard = ({
   id,
   name,
   description,
-  projectURL,
   technologies,
   updatedAt,
 }: IProjectData): JSX.Element => {
   return (
     <div className='h-48 w-full border-2 border-black p-1 rounded'>
-      <header className='h-1/6 w-full inline-flex justify-between'>
+      <header className='h-1/6'>
         <Link href={`/projects/${id}`}>
           <a className='cursor-pointer hover:opacity-60 overflow-x-hidden'>
             {name}
           </a>
         </Link>
-        <div className='text-xs flex flex-col'>
-          <span>Last update:</span>
-          <span>{updatedAt.slice(0, 7)}</span>
-        </div>
       </header>
-      <article className='h-4/6'>
-        <a className='text-xs' href={projectURL}>
-          {projectURL}
-        </a>
-        <p className='text-sm'>{description?.slice(0, 100)}</p>
+      <article className='h-3/6'>
+        <span className='h-1/6 text-xs'>
+          Last update: {updatedAt.slice(0, 7)}
+        </span>
+        <p className='h-5/6 text-sm tracking-tighter overflow-hidden'>
+          {description}
+        </p>
       </article>
-      <aside className='h-1/6'>
-        <div className='h-1/2 text-xs text-red-400 overflow-hidden'>
+      <aside className='h-2/6 w-full pt-2 overflow-hidden'>
+        <span className='text-xs break-words text-red-400'>
           {technologies.toString()}
-        </div>
+        </span>
       </aside>
     </div>
   )
