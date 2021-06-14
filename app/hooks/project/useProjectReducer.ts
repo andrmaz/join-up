68fc252537f55b17141be839b631fafce218d321
andrmaz <mazbsorz@gmail.com>
2021-06-14 22:41:51 +0200

    Removed unnecessary updateLocalStorage

--- app/hooks/project/useProjectReducer.ts
+++ app/hooks/project/useProjectReducer.ts
@@ -5,21 +5,12 @@ import type {ProjectActions, ProjectState} from 'app/types/project'
 
 const initialState = {projects: []}
 
-export function useProjectReducer(): readonly [
+export function useProjectReducer(): [
   ProjectState,
-  React.Dispatch<ProjectActions>,
-  React.EffectCallback
+  React.Dispatch<ProjectActions>
 ] {
   const [state, dispatch] = React.useReducer<
     React.Reducer<ProjectState, ProjectActions>
   >(projectReducer, initialState)
-  const {projects} = state
-  const serializedProjects = JSON.stringify(projects)
-  const updateLocalStorage = React.useCallback(() => {
-    document.cookie.includes('session')
-      ? window.localStorage.setItem('projects', serializedProjects)
-      : window.localStorage.removeItem('projects')
-  }, [serializedProjects])
-
-  return [state, dispatch, updateLocalStorage]
+  return [state, dispatch]
 }
