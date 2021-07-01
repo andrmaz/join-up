7a5f529999aa3300f60dbf99ba9f26fb57e32717
andrmaz <mazbsorz@gmail.com>
2021-07-01 17:22:17 +0200

    Customized Modal Component

--- app/components/notifications/Dialog/Alert.tsx
+++ app/components/notifications/Dialog/Alert.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import useRefCallback from '@hooks/ref/useRefCallback'
 
 import Portal from '@components/containers/Portal/Portal'
+import Modal from '@components/containers/Modal/Modal'
 import CancelButton from '@components/form/Button/Cancel'
 import {ConfirmButton} from '@components/form/Button/Confirm'
 import CloseButton from '@components/form/Button/Close'
@@ -24,51 +25,38 @@ const AlertDialog = ({
     <React.Fragment>
       {showDialog && (
         <Portal>
-          <section
-            id='dialog_layer'
-            className='fixed top-0 left-0 h-screen w-screen bg-gray-500 bg-opacity-50 z-50'
-          >
-            <article
-              id='dialog'
-              role='dialog'
-              aria-label='dialog'
-              aria-labelledby='dialog_label'
-              aria-modal={true}
-              aria-describedby='dialog_label'
-              className='fixed h-1/6 w-2/4 top-1/4 right-1/4 bg-white border-red-600 border-2 rounded p-4'
-            >
-              <div className='h-full w-full'>
-                <div className='w-full h-2/3'>
-                  <header className='h-1/2 flex justify-between'>
-                    <h2
-                      id='dialog_label'
-                      tabIndex={-1}
-                      ref={setRef}
-                      className='h-1/2 focus:ring-2 focus:ring-yellow-600 text-xl font-bold'
-                    >
-                      {title}
-                    </h2>
-                    <CloseButton
-                      onClickAction={() => setShowDialog(false)}
-                      focusRef={focusTrapRef}
-                    />
-                  </header>
-                  <span role='article' tabIndex={0} className='h-1/2'>
-                    {message}
-                  </span>
-                </div>
-                <div className='w-full h-1/3 flex'>
-                  <ConfirmButton bgColor='red' onClickAction={handleConfirm}>
-                    Confirm
-                  </ConfirmButton>
-                  <CancelButton
+          <Modal height='1/6' width='2/4' top='1/4' color='red-600'>
+            <div className='h-full w-full'>
+              <div className='w-full h-2/3'>
+                <header className='h-1/2 flex justify-between'>
+                  <h2
+                    id='dialog_label'
+                    tabIndex={-1}
+                    ref={setRef}
+                    className='h-1/2 focus:ring-2 focus:ring-yellow-600 text-xl font-bold'
+                  >
+                    {title}
+                  </h2>
+                  <CloseButton
                     onClickAction={() => setShowDialog(false)}
-                    onKeyDownAction={() => focusTrapRef.current?.focus()}
+                    focusRef={focusTrapRef}
                   />
-                </div>
+                </header>
+                <span role='article' tabIndex={0} className='h-1/2'>
+                  {message}
+                </span>
               </div>
-            </article>
-          </section>
+              <div className='w-full h-1/3 flex'>
+                <ConfirmButton bgColor='red' onClickAction={handleConfirm}>
+                  Confirm
+                </ConfirmButton>
+                <CancelButton
+                  onClickAction={() => setShowDialog(false)}
+                  onKeyDownAction={() => focusTrapRef.current?.focus()}
+                />
+              </div>
+            </div>
+          </Modal>
         </Portal>
       )}
     </React.Fragment>
